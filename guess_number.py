96d24651be46a51aa09d70b488bdf4be794e9968
Владислав Чернявский <cernavskijv877@gmail.com>
2024-05-15 21:05:12 +1000

    format document

--- guess_number.py
+++ guess_number.py
@@ -8,14 +8,15 @@ attempts = 0
 print('Угадайте число от 1 до 100!\n')
 
 while player_choise != str(computer_choise):
-    
     player_choise = input('Введите число: ')
-    try: int(player_choise)
-    except Exception: 
+    try:
+        int(player_choise)
+    except Exception:
         print('Можно указывать только цифры!')
         continue
-    
+
     if int(player_choise) > computer_choise:
+
         print('Ваше число больше того, что загадано')
     elif int(player_choise) < computer_choise:
         print('Ваше число меньше того, что загадано')
